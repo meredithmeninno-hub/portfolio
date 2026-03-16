1557be860e445a4526631e6b4820c6ea69f68f1b
msmeninno <msmeninno@ucdavis.edu>
2026-03-15 23:00:31 -0700

    resume sec

--- src/Homepage/Resume/Resume.jsx
+++ src/Homepage/Resume/Resume.jsx
@@ -15,7 +15,30 @@ export function Resume({paddingTop}){
         </div>
 
         <h3 className = {styles.title}>Skillset</h3>
-        <h3 className = {styles.subtitle}>DESIGN</h3>
+        <h3 className = {styles.subtitle}>Design</h3>
+        <div className = {styles.skills}>
+            <p>Adobe Illustrator</p>
+            <p>Adobe Photoshop</p>
+            <p>Adobe InDesign</p>
+            <p>Figma</p>
+            <p>Procreate</p>
+            <p>Blender</p>
+            <p>Rhino</p>
+        </div>
+
+        <h3 className = {styles.subtitle2}>Programming</h3>
+        <div className = {styles.skills}>
+            <p>HTML/CSS</p>
+            <p>Javascript</p>
+            <p>React</p>
+            <p>Python</p>
+            <p>Java</p>
+    
+        </div>
+
+
+         <h3 className = {styles.title}>Education</h3>
+        <Experience subtitle = "University of California, Davis" date="2022-2025" role="Bachelors of Arts, Design"/>
 
         </div>
     )
